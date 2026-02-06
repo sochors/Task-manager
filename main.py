283e5c5aa0fa12c923cd563f08cbf807da79b016
sochors <stepasoch@gmail.com>
2026-02-06 14:44:45 +0100

    small changes to code and test scenarios

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 ukoly = []
 
+
 def pridat_ukol():
     nazev_ukolu = input("Zadejte název úkolu: ")
     popis_ukolu = input("Zadejte popis úkolu: ")
-    ukoly.append(nazev_ukolu)
+    ukoly.append(f"{nazev_ukolu} – {popis_ukolu}")
     print(f"\nÚkol '{nazev_ukolu}' byl přidán\n")
     print(ukoly)
     hlavni_menu()
 
+
 def zobrazit_ukoly():
+    print("")
+
     for i, ukol in enumerate(ukoly, start=1):
         print(f"{i}. {ukol}")
 
+    print("")
     hlavni_menu()
 
 
 def odstranit_ukol():
-    task_number = int(input("Zadejte číslo úkolu, které chcete odstanit: "))
+    task_number = int(input(f"\nZadejte číslo úkolu, které chcete odstanit: "))
 
     # TO-DO Kontrola, že zadané číslo odpovídá počtu úkolů
     
@@ -37,7 +42,8 @@ def hlavni_menu():
     elif user_choice == "4":
         quit
     else:
-        print("Špatná volba.")
+        print(f"\nZvolili jste neplatnou možnost. Prosím, opakujte volbu.\n")
         hlavni_menu()
+
 
 hlavni_menu()
