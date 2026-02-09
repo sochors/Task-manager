ef1f98593e4d94275180733285382a9c660489d7
sochors <stepasoch@gmail.com>
2026-02-09 11:31:46 +0100

    small changes in code and test scenarios

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 ukoly = []
 
-
 def pridat_ukol():
     nazev_ukolu = input("Zadejte název úkolu: ")
     popis_ukolu = input("Zadejte popis úkolu: ")
     ukoly.append(f"{nazev_ukolu} – {popis_ukolu}")
     print(f"\nÚkol '{nazev_ukolu}' byl přidán\n")
-    print(ukoly)
+    print(ukoly) # TOTO PAK SMAZAT
     hlavni_menu()
 
 
 def zobrazit_ukoly():
-    print("")
+    if ukoly:
+        print("\nSeznam úkolů:")
+        for i, ukol in enumerate(ukoly, start=1):
+            print(f"{i}. {ukol}")
 
-    for i, ukol in enumerate(ukoly, start=1):
-        print(f"{i}. {ukol}")
-
-    print("")
-    hlavni_menu()
+        print("")
+        hlavni_menu()
+    else:
+        # print("")
+        print(f"\nMomentálně nemáš žádné přidané úkoly:)\n")
+        hlavni_menu()
 
 
 def odstranit_ukol():
-    task_number = int(input(f"\nZadejte číslo úkolu, které chcete odstanit: "))
-
-    # TO-DO Kontrola, že zadané číslo odpovídá počtu úkolů
-    
-    ukoly.remove(ukoly[task_number-1])
-    print(f"\nÚkol {task_number} byl odstraněn.\n")
-    hlavni_menu()
+    if ukoly:
+        task_number = int(input(f"\nZadejte číslo úkolu, které chcete odstanit: "))
+        # TO-DO Kontrola, že zadané číslo odpovídá počtu úkolů
+        ukoly.remove(ukoly[task_number-1])
+        print(f"\nÚkol {task_number} byl odstraněn.\n")
+        hlavni_menu()
+    else:
+        print(f"\nMomentálně nemáš žádné přidané úkoly:)\n")
+        hlavni_menu()
 
 
 def hlavni_menu():
@@ -40,6 +45,7 @@ def hlavni_menu():
     elif user_choice == "3":
         odstranit_ukol()
     elif user_choice == "4":
+        print(f"\nKonec programu.\n")
         quit
     else:
         print(f"\nZvolili jste neplatnou možnost. Prosím, opakujte volbu.\n")
