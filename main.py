c2d5daf489e2489d8b1a80b5fd8e2f3a9c3f9f7f
sochors <stepasoch@gmail.com>
2026-02-19 11:06:46 +0100

    fixed recursive calling of a function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 ukoly = []
 
 def pridat_ukol():
+    #TO-DO Upravit ukládání úkolů místo do seznamu do dict
+
     nazev_ukolu = input("Zadej název úkolu (pole nesmí být prázdné): ")
     while True:
         if not nazev_ukolu:
@@ -15,7 +17,6 @@ def pridat_ukol():
         else:
             ukoly.append(f"{nazev_ukolu} – {popis_ukolu}")
             print(f"\nÚkol '{nazev_ukolu}' byl přidán\n")
-            hlavni_menu()
             break
             
 
@@ -26,10 +27,8 @@ def zobrazit_ukoly():
             print(f"{i}. {ukol}")
 
         print("")
-        hlavni_menu()
     else:
         print(f"\nMomentálně nemáš žádné přidané úkoly:)\n")
-        hlavni_menu()
 
 
 def odstranit_ukol():
@@ -39,35 +38,35 @@ def odstranit_ukol():
             print(f"{i}. {ukol}")
             
         task_number = int(input(f"\nZadej číslo úkolu, které chceš odstranit: "))
-        
+
+        #TO-DO: Kontrola, že je vstup int a ne třeba str
+                
         while True:
             if task_number in range(1, len(ukoly) + 1):
                 ukoly.remove(ukoly[task_number-1])
                 print(f"\nÚkol {task_number} byl odstraněn.\n")
-                hlavni_menu()
                 break
             else:
                 task_number = int(input(f"\nNeplatná hodnota. Zadej číslo úkolu, které chceš odstranit: "))
     else:
         print(f"\nMomentálně nemáš žádné přidané úkoly:)\n")
-        hlavni_menu()
 
 
 def hlavni_menu():
-    user_choice = input(f"Správce úkolů - Hlavní menu\n1. Přidat nový úkol\n2. Zobrazit všechny úkoly\n3. Odstranit úkol\n4. Konec programu\nVyberte možnost(1-4) ")
+    while True:
+        user_choice = input(f"Správce úkolů - Hlavní menu\n1. Přidat nový úkol\n2. Zobrazit všechny úkoly\n3. Odstranit úkol\n4. Konec programu\nVyberte možnost(1-4) ")
 
-    if user_choice == "1":
-        pridat_ukol()
-    elif user_choice == "2":
-        zobrazit_ukoly()
-    elif user_choice == "3":
-        odstranit_ukol()
-    elif user_choice == "4":
-        print(f"\nKonec programu.\n")
-        quit()
-    else:
-        print(f"\nZvolil/a jsi neplatnou možnost. Prosím, opakuj volbu.\n")
-        hlavni_menu()
+        if user_choice == "1":
+            pridat_ukol()
+        elif user_choice == "2":
+            zobrazit_ukoly()
+        elif user_choice == "3":
+            odstranit_ukol()
+        elif user_choice == "4":
+            print(f"\nKonec programu.\n")
+            break
+        else:
+            print(f"\nZvolil/a jsi neplatnou možnost. Prosím, opakuj volbu.\n")
 
 
 hlavni_menu()
